7d73ab94e1049582de68dae7e57fa23f00c413c4
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-02-20 15:32:59 +0100

    changed name of localhost server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  let server = app.listen(3000,"0.0.0.0",() => {
+  let server = app.listen(3000,"localhost",() => {
      let host = server.address().address;
      let port = server.address().port;
      console.log("Servidor escuchando en http://%s:%s", host, port);
